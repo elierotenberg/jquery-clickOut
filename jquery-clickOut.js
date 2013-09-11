020be71113a72789d4d27682bc0eac1fac036444
elierotenberg <elie.rotenberg@gmail.com>
2013-09-11 04:14:58 +0200

    Update jquery-clickOut.js

--- jquery-clickOut.js
+++ jquery-clickOut.js
@@ -1,10 +1,12 @@
 (function($) {
     "use strict";
     $.fn.clickout = function(callback) {            // Triggers the callback the next time a click event occurs OUTSIDE of the target, and then unbinds the click listener.
-        this.each(function(el) {
+        this.each(function() {
+            var el = this;
             var clickOutHandler = function(e) {
                 if(!$.contains(el, e.target)) {
                     $(document).off("click", clickOutHandler);
+                    callback.call(el, e);
                 }
             };
             $(document).on("click", clickOutHandler);
